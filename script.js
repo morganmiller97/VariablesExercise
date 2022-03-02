b67c4ebc1018bdd67be989bc38ceeff17d11980b
“morganmiller97” <adams_morgan97@icloud.com>
2022-03-02 10:01:58 -0500

    VariablesExercise

---
+++ script.js
@@ -0,0 +1,47 @@
+//VARIABLES EXERCISE
+// 1
+//const
+
+// 2
+let num = 20;
+
+// 3
+num = 15;
+
+// 4
+num = num + 7;
+
+// 5
+num--;
+
+// 6
+num = "This is not a number.";
+
+// 7
+let blank;
+
+// 8
+console.log(blank);
+
+// 9
+blank = nul;
+
+//BONUS
+// 1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+
+// 2
+console.log(quarter + dime + nickel + penny);
+
+//3
+let letters = "qwertyiop";
+
+// 4
+console.log(letter.length);
+console.log(letters[9]); //p
+
+
+
+
+
+
